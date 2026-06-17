c2f82837bddc3b1605a45760a4a850268687d1b4
Lia S <eopqrs@gmail.com>
2026-06-17 11:36:55 +0100

    1st milestone: 3 parts dashboard layout, initial media query added

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ The React Compiler is not enabled on this template because of its impact on dev 
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+--src/
+├── components/
+│   ├── Dashboard.jsx (main container)
+│   └── TaskTable.jsx (we'll build this next)
+├── data/
+│   └── mockTasks.js (mock data)
+├── App.jsx
+└── styles/
+    └── dashboard.css
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import './styles/app.scss';
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <header>
-        <h1>Operation Dashboard</h1>
-        <p>Internal task tracking for staff workflows</p>
-      </header>
-      <aside>
-        <h3>Filter</h3>
-        <p>All</p>
-        <p>To-Do</p>
-        <p>In Progress</p>
-        <p>Done</p>
-        <p>Blocked</p>
-      </aside>
-      <main>
-        <h3>Tasks</h3>
-        Table will go here next milestone
-        For now, use an empty placeholder box or text block
-      </main>
+      <div className="dashboard">
+        <header>
+          <h1>Operations Dashboard</h1>
+          <p><i>Internal task tracking for staff workflows</i></p> {/*note to self*/}
+          <p>
+            {/* Right-aligned: small user badge  */}
+            name + avatar placeholder
+          </p>
+        </header>
+        <aside>
+          <h3>Filter</h3>
+          <div className="button-wrapper">
+            <button>All</button>
+            <button>To-Do</button>
+            <button>In Progress</button>
+            <button>Done</button>
+            <button>Blocked</button>
+          </div>
+        </aside>
+        <main>
+          <h3>Tasks</h3>
+          Table will go here next milestone
+          For now, use an empty placeholder box or text block
+        </main>
+      </div>
     </>
   )
 }
